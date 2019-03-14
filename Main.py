49789ad649c1ad47e2eb3b608cf3ed990158cb38
Sheheryar <sheheryarkhalid39@gmail.com>
2019-03-14 21:58:03 +0500

    Exercise3.1. Writeafunctionnamed right_justify thattakesastringnamed s asaparameter and prints the string with enough leading spaces so that the last letter of the string is in column 70 of the display

--- Main.py
+++ Main.py
@@ -1,1 +1,12 @@
-import math
+import sys
+# Exercise3.1. Writeafunctionnamed right_justify thattakesastringnamed s asaparameter and prints the string with enough leading spaces so that the last letter of the string is in column 70 of the display
+
+
+def right_justify(string_to_be_justified):
+    """
+    string_to_be_justified: String which needs to be right justified
+    """
+    empty_space = " " * (70 - len(string_to_be_justified))
+    justified_string = empty_space+string_to_be_justified + 'f'
+    print(justified_string)
+
